37600b7bec279206a9a2fea5e7b0c3f99e7a6d3a
dark-night-men <visible.ocean@gmail.com>
2015-08-30 20:17:22 +0300

    Multimatch draft version

--- logreader/ilogfile.cpp
+++ logreader/ilogfile.cpp
@@ -3,7 +3,126 @@
 #include <QDebug>
 #include <assert.h>
 
-#include <regex.h>        
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <regex.h>
+
+/* The following is the size of a buffer to contain any error messages
+   encountered when the regular expression is compiled. */
+
+#define MAX_ERROR_MSG 0x1000
+
+/* Compile the regular expression described by "regex_text" into
+   "r". */
+
+static int compile_regex (regex_t * r, const char * regex_text)
+{
+    int status = regcomp (r, regex_text, REG_EXTENDED|REG_NEWLINE);
+    if (status != 0) {
+	char error_message[MAX_ERROR_MSG];
+	regerror (status, r, error_message, MAX_ERROR_MSG);
+        printf ("Regex error compiling '%s': %s\n",
+                 regex_text, error_message);
+        return 1;
+    }
+    return 0;
+}
+
+/*
+  Match the string in "to_match" against the compiled regular
+  expression in "r".
+ */
+
+static int match_regex (regex_t * r, const char * to_match)
+{
+
+    logprint("match_regex started");
+    /* "P" is a pointer into the string which points to the end of the
+       previous match. */
+    const char * p = to_match;
+    /* "N_matches" is the maximum number of matches allowed. */
+    const int n_matches = 10;
+    /* "M" contains the matches found. */
+    regmatch_t m[n_matches];
+
+    while (1) {
+        int i = 0;
+        int nomatch = regexec (r, p, n_matches, m, 0);
+        if (nomatch) {
+            //printf ("match_regex No more matches.\n");
+            logprint ("match_regex No more matches.\n");
+            return nomatch;
+        }
+        for (i = 0; i < n_matches; i++) {
+            int start;
+            int finish;
+            if (m[i].rm_so == -1) {
+                break;
+            }
+            start = m[i].rm_so + (p - to_match);
+            finish = m[i].rm_eo + (p - to_match);
+
+            /*
+            if (i == 0) {
+                printf ("$& is ");
+            }
+            else {
+                printf ("$%d is ", i);
+            }
+            printf ("'%.*s' (bytes %d:%d)\n", (finish - start),
+                    to_match + start, start, finish);
+                    */
+            QString prefix = "match_regex " ;
+            if (i == 0) {
+                prefix += " is ";
+            }
+            else {
+                prefix += QString("%1 is ").arg ( i);
+            }
+
+            int length = finish - start;
+
+            char* otherString = 0;
+
+            otherString = (char*)malloc(length+1);
+            memcpy(otherString,to_match + start,length);
+            otherString[length] = 0;
+
+            logprint(prefix + QString(">%1< (bytes %2:%3)\n").arg(otherString).arg(start).arg(finish));
+            free(otherString);
+
+        }
+        p += m[0].rm_eo;
+    }
+    logprint("match_regex exit");
+    return 0;
+}
+
+/*
+int main(int argc, char ** argv)
+{
+    regex_t r;
+    const char * regex_text;
+    const char * find_text;
+    if (argc != 3) {
+        regex_text = "([[:digit:]]+)[^[:digit:]]+([[:digit:]]+)";
+        find_text = "This 1 is nice 2 so 33 for 4254";
+    }
+    else {
+        regex_text = argv[1];
+        find_text = argv[2];
+    }
+    printf ("Trying to find '%s' in '%s'\n", regex_text, find_text);
+    compile_regex(& r, regex_text);
+    match_regex(& r, find_text);
+    regfree (& r);
+    return 0;
+}
+*/
+
+
+
 
 IReader::IReader(const QString & filename, ILogFile* ilogFile )
     :QFile(filename)
@@ -181,15 +300,23 @@ void IFinder::find1( const QString & pattern )
     /* Compile regular expression */
     //reti = regcomp(&regex, "^a[[:alnum:]]", 0);
     const char * lpattern = pattern.toLatin1().constData();
-    reti = regcomp(&regex, lpattern , 0);
+    //reti = regcomp(&regex, lpattern , 0);
+    reti = compile_regex(& regex, lpattern);
     if (reti) {
         fprintf(stderr, "Could not compile regex\n");
         exit(1);
     }
 
+
+
+
     /* Execute regular expression */
     //reti = regexec(&regex, "abc", 0, NULL, 0);
     const char* pointerToText = ilogFile_->content().first().constData();
+
+    match_regex(& regex, pointerToText );
+
+    /*
     reti = regexec(&regex, pointerToText, 0, NULL, 0);
     if (!reti) {
         puts("Match");
@@ -204,9 +331,12 @@ void IFinder::find1( const QString & pattern )
         fprintf(stderr, "Regex match failed: %s\n", msgbuf);
         exit(1);
     }
+    */
 
     /* Free compiled regular expression if you want to use the regex_t again */
     regfree(&regex);
+
+    emit resultReady(QString("IFinder::find1 result %1").arg(pattern));
 }
 
 
